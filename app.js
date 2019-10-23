70e1e0bac3db52190c381ff81c56533b0d84534c
huynvq <huy.nguyen@fssc.com.vn>
2019-10-23 13:09:34 +0700

    update write db

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const LOG_TAG = 'app';
 const adapter = new FileSync('db.json');
 const db = lowdb(adapter);
 require('dotenv').config();
+const dbName = 'days';
 
 const app = express();
 
@@ -27,12 +28,14 @@ async function editTs() {
     return new Promise(async function (resolve, reject) {
         try {
             var key = DateTimeUtils.convertTsToDate(Date.now());
-            if (db.has(key).value()) {
+
+            var writedDate = db.get(dbName).value();
+            if (writedDate.includes(key)) {
                 return resolve();
             };
             logger.log('info', `${LOG_TAG}-editTs-START`);
             var loginResult = await sendLoginRequest();
-            logger.log('info', loginResult);
+            logger.log('info', `${LOG_TAG}-editTs-loginResult: ${loginResult}`);
             var moningRes = await editTimeSheet(loginResult.headers['set-cookie'], { isMorning: true });
             var afternRes = await editTimeSheet(loginResult.headers['set-cookie'], { isMorning: false });
             // logger.log('info', 'write success at ' + DateTimeUtils.convertTsToDate(Date.now()));
@@ -45,7 +48,11 @@ async function editTs() {
                 logger.log('error', `${LOG_TAG}-editTs-error: No response edit timesheet`);
                 return reject(new Error('Login Failed'));
             };
-            db.set(key, []).write();
+
+            db.get(dbName)
+                .push(key)
+                .write();
+
             return resolve();
         } catch (error) {
             logger.log('error', `${LOG_TAG}-editTs-error: ${error}`);
@@ -57,6 +64,9 @@ async function editTs() {
 app.listen(8000, function () {
     console.log('Running on port 8000!');
     var { pattern } = config.scheduler;
+    var dbObj = {};
+    dbObj[dbName] = [];
+    db.defaults(dbObj).write();
     cron.schedule(pattern, async function () {
         try {
             await editTs();
